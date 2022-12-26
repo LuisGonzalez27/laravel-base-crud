4efd2aceb741a0e3b25b640cd89a8db1a793a341
LuisGonzalez27 <luis27coding@gmail.com>
2022-12-26 10:38:27 +0100

    Sistemazione button

--- resources/views/gifts/create.blade.php
+++ resources/views/gifts/create.blade.php
@@ -25,14 +25,12 @@
                 </div>
             </div>
 
-            
-    <div class="col-6">
+            <div class="col-6">
+                <div class="d-flex flex-column align-items-center mb-4">
+                    <label for="age">Età</label>
+                    <input required type="text" name="age" id="age" placeholder="età" class="p-1">
+                </div>
 
-        <div class="d-flex flex-column align-items-center mb-4">
-            <label for="age">Età</label>
-            <input required type="text" name="age" id="age" placeholder="età" class="p-1">
-        </div>
-    
                 <div class="d-flex flex-column align-items-center mb-4">
                     <label for="country">Paese</label>
                     <input required type="text" name="country" id="country"  placeholder="nazione" class="p-1">
@@ -42,35 +40,32 @@
                     <label for="city">Città</label>
                     <input required type="text" name="city" id="city" placeholder="città" class="p-1">
                 </div>
-        
-    </div>
+            </div>
 
-    <div class="col-6 ">
-        
+            <div class="col-6 ">
                 <div class="d-flex flex-column align-items-center mb-4">
                     <label for="address">Indirizzo</label>
                     <input required type="text" name="address" id="address"  placeholder="indirizzo" class="p-1">
                 </div>
-        
-    </div>
+            </div>
 
-    <div class="col-6 ">
-        <div class="d-flex flex-column align-items-center mb-4">
-            <label for="image">Immagine</label>
-            <select required name="image" id="image" class="p-1">
-                <option value="https://www.labottegadelnerd.com/labottegadelnerd/wp-content/uploads/2020/11/gift-box-png-transparent-21.png" selected>regalo</option>
-                <option value="https://www.pngall.com/wp-content/uploads/8/Coal-PNG-Pic.png">carbone</option>
-            </select>
-            {{-- <input required type="text" name="type" id="type"  placeholder="Inserisci il tipo" class="p-1"> --}}
-        </div>
-    </div>
+            <div class="col-6 ">
+                <div class="d-flex flex-column align-items-center mb-4">
+                    <label for="image">Immagine</label>
+                    <select required name="image" id="image" class="p-1">
+                        <option value="https://www.labottegadelnerd.com/labottegadelnerd/wp-content/uploads/2020/11/gift-box-png-transparent-21.png" selected>regalo</option>
+                        <option value="https://www.pngall.com/wp-content/uploads/8/Coal-PNG-Pic.png">carbone</option>
+                    </select>
+                </div>
+            </div>
 
-    <div class="col">
-        <input type="submit" value="Invia" class="btn submit-btn">
-    </div>
-    
-         </form>
-         
+            <div class="col">
+                <input type="submit" value="Invia" class="btn submit-btn">
+                <div class="mt-3">
+                    <a class="text-uppercase text-white col-12 text-decoration-underline " href="{{route('gifts.index')}}">back to gifts</a>
+                </div>
+            </div>
+        </form>    
     </div>
     
 </section>
--- resources/views/gifts/show.blade.php
+++ resources/views/gifts/show.blade.php
@@ -35,10 +35,11 @@
                 <button class="noselect delete"><span class='text'>Elimina</span><span class="icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 20.188l-8.315-8.209 8.2-8.282-3.697-3.697-8.212 8.318-8.31-8.203-3.666 3.666 8.321 8.24-8.206 8.313 3.666 3.666 8.237-8.318 8.285 8.203z"/></svg></span></button>
                 </form>
             </div>
+            <div class="mt-3">
+                <a class="text-uppercase text-white col-12 text-decoration-underline " href="{{route('gifts.index')}}">back to gifts</a>
+            </div>
         </div>
-      
     </div>
-    <div class="mt-3"><a class="text-uppercase text-white col-12 text-decoration-underline " href="{{route('gifts.index')}}">back to gifts</a></div>
 </section>   
 
 
